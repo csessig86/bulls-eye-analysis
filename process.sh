f14248dfa548ff06c5220e035606398be934d46a
csessig86 <csessig86@gmail.com>
2018-05-03 15:05:48 -0500

    add county fps

--- process.sh
+++ process.sh
@@ -12,8 +12,8 @@ param=${params#$prefix}
 IFS=', ' read -r -a params_array <<< ${param}
 
 
-YEARS=( 15 13 12 11 10 00 )
-# YEARS=( 10 )
+# YEARS=( 15 13 12 11 10 00 )
+YEARS=( 00 )
 
 if [[ " ${params_array[*]} " != *" merge "* ]]; then
   cd raw_data/census_tracts
@@ -28,7 +28,7 @@ if [[ " ${params_array[*]} " != *" merge "* ]]; then
       RAW_TRACTS="tr48_d${year}_concat"
       RAW_POP="DEC_${year}_SF1_P001"
 
-      ogr2ogr -dialect sqlite -sql "select STATE || COUNTY || CASE WHEN length(TRACT) = 4 THEN TRACT || '00' ELSE TRACT END as GEOID, geometry FROM $RAW_TRACTS_1" $RAW_TRACTS.shp $RAW_TRACTS_1.shp -progress
+      ogr2ogr -dialect sqlite -sql "select COUNTY, STATE || COUNTY || CASE WHEN length(TRACT) = 4 THEN TRACT || '00' ELSE TRACT END as GEOID, geometry FROM $RAW_TRACTS_1" $RAW_TRACTS.shp $RAW_TRACTS_1.shp -progress
     elif [[ " ${year} " == *" 10 "* ]]; then
       RAW_TRACTS="tl_20${year}_48_tract10"
       RAW_POP="DEC_${year}_SF1_P1"
@@ -53,12 +53,14 @@ if [[ " ${params_array[*]} " != *" merge "* ]]; then
     ogr2ogr -sql "select $RAW_TRACTS.*, $RAW_POP.* from $RAW_TRACTS left join '$RAW_POP.csv'.$RAW_POP on $RAW_TRACTS.GEOID$YR_ADD = $RAW_POP.GEO_id2" $EDIT_ONE.shp $RAW_TRACTS.shp -progress
   done
 
+  # Move newly created files to correct directory
+  mv edit_* ../../edits
   cd ../../
 fi
 
 
 if [[ " ${params_array[*]} " != *" filter "* ]]; then
-  cd raw_data/census_tracts
+  cd edits/
 
   for year in "${YEARS[@]}";
   do
@@ -67,14 +69,42 @@ if [[ " ${params_array[*]} " != *" filter "* ]]; then
     EDIT_TWO="edit_b_tracts_pop_format_${year}"
 
     echo "Select columns, convert some to integers"
-    if [[ " ${year} " == *" 00 "* ]] || [[ " ${year} " == *" 10 "* ]] ; then
-      ogr2ogr -sql "SELECT DEC_${year}_SF1 as geo_id, DEC_${year}_S_1 as geo_id_two, DEC_${year}_S_2 as geo_label, cast(DEC_${year}_S_3 as float(10,0)) as total_pop FROM $EDIT_ONE" $EDIT_TWO.shp $EDIT_ONE.shp -progress
+    if [[ " ${year} " == *" 00 "* ]]; then
+      ogr2ogr -sql "SELECT DEC_${year}_SF1 as geo_id, DEC_${year}_S_1 as geo_id_two, DEC_${year}_S_2 as geo_label, tr48_d${year}_c as county_fps, cast(DEC_${year}_S_3 as float(10,0)) as total_pop FROM $EDIT_ONE" $EDIT_TWO.shp $EDIT_ONE.shp -progress
+    elif [[ " ${year} " == *" 10 "* ]] ; then
+      ogr2ogr -sql "SELECT DEC_${year}_SF1 as geo_id, DEC_${year}_S_1 as geo_id_two, DEC_${year}_S_2 as geo_label, tl_20${year}__1 as county_fps, cast(DEC_${year}_S_3 as float(10,0)) as total_pop FROM $EDIT_ONE" $EDIT_TWO.shp $EDIT_ONE.shp -progress
     else
-      ogr2ogr -sql "SELECT ACS_${year}_5YR as geo_id, ACS_${year}_5_1 as geo_id_two, ACS_${year}_5_2 as geo_label, cast(ACS_${year}_5_3 as float(10,0)) as total_pop, cast(ACS_${year}_5_4 as float(10,0)) as moe FROM $EDIT_ONE" $EDIT_TWO.shp $EDIT_ONE.shp -progress
+      ogr2ogr -sql "SELECT ACS_${year}_5YR as geo_id, ACS_${year}_5_1 as geo_id_two, ACS_${year}_5_2 as geo_label, tl_20${year}__1 as county_fps, cast(ACS_${year}_5_3 as float(10,0)) as total_pop, cast(ACS_${year}_5_4 as float(10,0)) as moe FROM $EDIT_ONE" $EDIT_TWO.shp $EDIT_ONE.shp -progress
     fi
   done
 
-  # Move newly created files to correct directory
-  mv edit_* ../../edits
+  cd ../../
+fi
+
+if [[ " ${params_array[*]} " != *" random-points "* ]]; then
+  cd edits/
+
+  for year in "${YEARS[@]}";
+  do
+    echo "Current year: ${year}"
+    EDIT_TWO="edit_b_tracts_pop_format_${year}"
+    EDIT_THREE="edit_c_random_points_${year}"
+
+    echo "Create PostgreSQL db called db${year} and append shapefile to it"
+    # createdb bulls_eye
+    psql -d bulls_eye -c "DROP TABLE db${year};"
+    psql -d bulls_eye -c "CREATE TABLE db${year}();"
+    shp2pgsql -c $EDIT_TWO.shp db${year} | psql -d bulls_eye
+
+    # ogr2ogr -append -f "PostgreSQL" PG:"bulls_eye=db${year}" $EDIT_TWO.shp -nln $EDIT_TWO 
+    
+    # echo "Query db"
+    # pgsql2shp -f $EDIT_THREE db${year} "SELECT RandomPointsInPolygon($EDIT_TWO.geom, 'total_pop') AS random_points FROM $EDIT_TWO"
+    
+    # psql -d db${year} -c "SELECT RandomPointsInPolygon($EDIT_TWO.geom, 'total_pop') AS manypoints FROM $EDIT_TWO"
+    # ogr2ogr -f "PostgreSQL" PG:"dbname=db" spatialitedb -sql "SELECT * FROM table" -dialect spatialite -nln new_table
+    # ogr2ogr -dialect spatialite -sql "SELECT RandomPointsInPolygon($EDIT_TWO.geom, 'total_pop') AS manypoints FROM $EDIT_TWO" $EDIT_THREE.shp $EDIT_TWO.shp -progress
+  done
+
   cd ../../
 fi
